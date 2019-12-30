6740bfd8c6d1b669b4fde815bde428ec71fb118f
JesseTCS <jt873@scarletmail.rutgers.edu>
2019-12-30 18:22:25 -0500

    Django Test: Is the hash of hello correct

--- hashthat_project/hashing/tests.py
+++ hashthat_project/hashing/tests.py
@@ -15,6 +15,7 @@ class FunctionalTest(TestCase):
         Hook method for setting up the test fixture before exercising it.
 
         :var webdriver self.safari: Used to operate the safari web-browser
+
         :return: None
         """
         self.safari = webdriver.Safari()
@@ -23,16 +24,51 @@ class FunctionalTest(TestCase):
         """
         This function is used to test if there is a homepage.
 
-        :var string localhost_8000: Holds the localhost url with port number
         :var string test_text: Hold what string you would like tested
         :except assertion: Raised if test_text is not in the page source
+
         :return: None
         """
-        localhost_8000 = 'http://localhost:8000'
-        self.safari.get(localhost_8000)
+        self.__open_homepage()
 
-        test_text = 'install'
+        test_text = 'Enter hash here:'
         self.assertIn(test_text, self.safari.page_source)
+
+    def test_hash_of_hello(self) -> None:
+        """
+        This function inputs the text 'hello' and checks if the output hash is correct
+
+        :var string element_id: Contains an element id in the form of a string
+        :var string test_text: Contains a string to input
+        :var string element_name: Contains an element name in the form of a string
+        :var string hash_test: Contains a hash value in the form of a string
+
+        :return: None
+        """
+        self.__open_homepage()
+
+        element_id = 'id_text'
+        text = self.safari.find_element_by_id(element_id)
+
+        test_text = 'hello'
+        text.send_keys(test_text)
+
+        element_name = 'submit'
+        self.safari.find_element_by_name(element_name).click()
+
+        hash_test = '2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824'
+        self.assertIn(hash_test, self.safari.page_source)
+
+    def __open_homepage(self) -> None:
+        """
+        This function opens the homepage.
+
+        :var string url: Holds the url with port number as a string
+
+        :return: None
+        """
+        url = 'http://localhost:8000'
+        self.safari.get(url)
 
     def tearDown(self) -> None:
         """
@@ -42,8 +78,6 @@ class FunctionalTest(TestCase):
         """
         self.safari.quit()
 
-# def func_test():
-#     """
-#     :var webdriver safari: Used to operate the safari web-browser
-#     :return: nothing
-#     """
+
+class UnitTest(TestCase):
+    pass
